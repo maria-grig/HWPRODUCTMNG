dc760790272c525afadbab01eb0897bb9c84ba6e
MariaG <MGrigorenko@bellintegrator.com>
2022-11-13 22:48:09 +0300

    reformat code

--- src/main/java/ru/netology/manager/Smartphone.java
+++ src/main/java/ru/netology/manager/Smartphone.java
@@ -3,7 +3,7 @@ package ru.netology.manager;
 public class Smartphone extends Product {
     private String producer;
 
-    public Smartphone(int id, String name, int price, String producer) {
+        public Smartphone(int id, String name, int price, String producer) {
         super(id, name, price);
         this.producer = producer;
     }
